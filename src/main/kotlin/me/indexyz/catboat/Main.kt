31ef90149611c7cc4846a895d918244813fb234a
Indexyz <jiduye@gmail.com>
2020-06-28 03:22:07 +0800

    feat(bilibili): add parse for b23.tv

--- src/main/kotlin/me/indexyz/catboat/Main.kt
+++ src/main/kotlin/me/indexyz/catboat/Main.kt
@@ -1,12 +1,9 @@
 package me.indexyz.catboat
 
-import com.alibaba.fastjson.JSONObject
 import me.indexyz.catboat.process.BilibiliRichMessage
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.alsoLogin
-import net.mamoe.mirai.event.subscribeMessages
 import net.mamoe.mirai.join
-import net.mamoe.mirai.message.data.RichMessage
 
 val username = System.getenv("BOT_USERNAME").toLong()
 val password = System.getenv("BOT_PASSWORD")!!
@@ -16,18 +13,7 @@ suspend fun main() {
         fileBasedDeviceInfo("device.json")
     }.alsoLogin()
 
-    bot.messageDSL()
+    BilibiliRichMessage.register(bot)
 
     bot.join()
-}
-
-fun Bot.messageDSL() {
-    this.subscribeMessages {
-        has<RichMessage> {
-            val data = JSONObject.parseObject(it.content)
-            if (BilibiliRichMessage.isTargetMessage(data)) {
-                BilibiliRichMessage.onMessage(data, this)
-            }
-        }
-    }
 }
--- src/main/kotlin/me/indexyz/catboat/process/BilibiliRichMessage.kt
+++ src/main/kotlin/me/indexyz/catboat/process/BilibiliRichMessage.kt
@@ -4,9 +4,36 @@ import com.alibaba.fastjson.JSONObject
 import me.indexyz.catboat.utils.getBVFromUrl
 import me.indexyz.catboat.utils.parseUrl
 import me.indexyz.catboat.utils.queryVideo
+import net.mamoe.mirai.Bot
+import net.mamoe.mirai.event.subscribeMessages
 import net.mamoe.mirai.message.MessageEvent
+import net.mamoe.mirai.message.data.RichMessage
 
 object BilibiliRichMessage {
+    fun register(bot: Bot) {
+        bot.subscribeMessages {
+            has<RichMessage> {
+                val data = JSONObject.parseObject(it.content)
+                if (BilibiliRichMessage.isTargetMessage(data)) {
+                    BilibiliRichMessage.onMessage(data, this)
+                }
+            }
+
+            Regex("(https://b23\\.tv/[\\w]{6})") finding {
+                if (it.groups.isNotEmpty()) {
+                    val bv = parseUrl(it.groups[0]!!.value)?.let { it1 -> getBVFromUrl(it1) }
+                    bv?.let { it1 ->
+                        val video = queryVideo(it1)
+                        if (video != null) {
+                            val str = video.ReadableString()
+                            this.reply(str)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     fun isTargetMessage(message: JSONObject): Boolean {
         if (!message.containsKey("appID")) {
             return false
